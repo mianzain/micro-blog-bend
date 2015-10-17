14c539f9a7ec0fa0cd998455de638ce6943b2b29
mianzain <zainahmed@Zains-MacBook-Air.local>
2015-10-17 13:55:08 -0700

    initial express server

---
+++ server.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var server = app.listen(3000, function () {
+  var port = server.address().port;
+  console.log('app listening on port %s', port);
+});
